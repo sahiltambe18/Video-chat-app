87ec469018615e3a3dda7fc6fd3d702fd69e4976
Sahil Tambe <sahiltambe81922@gmail.com>
2023-07-11 20:27:45 +0530

    ui working

--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -7,15 +7,15 @@ const VideoPlayer = () => {
   const context = useContext(SocketContext);
 
   return (
-    <div className="flex flex-col space-y-5 lg:flex-row sm:flex-row sm:space-x-11">
+    <div className="flex flex-col space-y-5 lg:flex-row sm:flex-row sm:space-y-0 sm:space-x-11">
       {context.myVideo &&
-          <div>
+          <div className="videoPlayer">
             <h5>{context.name || 'Name'} </h5>
             <video playsInline muted ref={context.myVideo} autoPlay />
           </div>
       }
       {context.callAccepted && !context.callEnded &&
-        <div>
+        <div className="videoPlayer">
             <h5>{context.call.name || "Name"} </h5>
             <video playsInline ref={context.userVideo} autoPlay />
           </div>
